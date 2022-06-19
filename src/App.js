bb6f81b01f3a4c9ceec7f9c583e4e25dbfe03ab7
ANDREA GOMEZ <gomezandrea@hotmail.fr>
2022-06-19 19:03:43 +0200

    add js + css files

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [users, setUsers] = useState();
+
+  // Function to collect data
+  const getApiData = async () => {
+    const response = await fetch(
+      "https://simpsons-quotes-api.herokuapp.com/quotes"
+    ).then((response) => response.json());
+
+    setUsers(response);
+  };
+
+  useEffect(() => {
+    getApiData();
+  }, []);
+
+  return (
+    <div className="app">
+      {users &&
+        users.map((user) => (
+          <div className="item-container">
+            Citation : {user.quote}{" "}
+            <div className="title"> Personnage : {user.character}</div>
+            <div className="image-container">
+              <img src={user.image} alt="image" className="image" />
+            </div>
+            <button className="button" onClick={getApiData}>
+              Cliquer ici
+            </button>
+          </div>
+        ))}
+    </div>
+  );
+}
+
+export default App;
